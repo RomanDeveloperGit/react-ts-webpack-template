8b559ee10b800d034b650afb224758479d2b0e7b
roman_developer <roman.developer17@gmail.com>
2022-12-20 08:37:16 +0500

    Init template

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+import './assets/styles/index.scss';
+
+const App: FC = () => {
+  console.log('App');
+
+  return (
+    <div>
+      <div>App</div>
+    </div>
+  );
+};
+
+export default App;
